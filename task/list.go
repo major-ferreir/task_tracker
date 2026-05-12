6d7a692d5e498e9653120865c806665218c2a3aa
major-ferreir <edmiro952@gmail.com>
2026-05-12 08:33:26 +0100

    Formating List and writing the update command

--- task/list.go
+++ task/list.go
@@ -2,23 +2,44 @@ package task
 
 import (
 	"fmt"
-	"encoding/json"
+	"strings"
 )
 
 func ListAllTasks(taskList []Task) {
 
-	data, err := json.MarshalIndent(taskList, "", " ")
-	if err != nil {
-		panic(err)
-	}
+	// data, err := json.MarshalIndent(taskList, "", " ")
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
 	if len(taskList) == 0 {
 		fmt.Println("No task were added!")
 		return
 	}
 
-	fmt.Println(string(data)) 
+	for i, task := range taskList {
 
+		if i > 0 {
+			fmt.Println(strings.Repeat("-", 40))
+		}
+
+		fmt.Println("Task Id:", task.Id)
+		fmt.Println("Task Name: " + task.TaskName)
+		fmt.Println("Task Description:")
+		fmt.Println(task.Description)
+		fmt.Print("Task Status: ")
+
+		switch task.Status {
+		case 0:
+			fmt.Println("To-do")
+		case 1:
+			fmt.Println("In Progress")
+		case 2:
+			fmt.Println("Done")
+		}
+	}
+
+	fmt.Println(strings.Repeat("-", 40))
 }
 
 func ListTaskByStatus(status string, taskList []Task){
@@ -37,10 +58,22 @@ func ListTaskByStatus(status string, taskList []Task){
 		return 
 	}
 
-	for _, task := range taskList {
+	notFound := true
+
+	for i, task := range taskList {
 		if task.Status == localStatus {
+			if i > 0 {
+				fmt.Println(strings.Repeat("-", 40))
+				notFound = false
+			}
+			fmt.Println("Task Id:", task.Id)
 			fmt.Println("Task name: " + task.TaskName)
 			fmt.Println("Description: " + task.Description)
 		}
 	}
+
+	if notFound == false {
+		fmt.Println(strings.Repeat("-", 40))
+	}
+
 }
--- task/update.go
+++ task/update.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"encoding/json"
 	"os"
+	"bufio"
 )
 
 func UpdateTask(taskId int, newTaskName string, taskList *[]Task) {
@@ -11,6 +12,14 @@ func UpdateTask(taskId int, newTaskName string, taskList *[]Task) {
 	for i, task := range *taskList {
 		if task.Id == taskId {
 			(*taskList)[i].TaskName = newTaskName
+			scanner := bufio.NewScanner(os.Stdin)
+			fmt.Print("Description: ")
+			scanner.Scan()
+			description := scanner.Text()
+
+			if description != "" {
+				(*taskList)[i].Description = description
+			}
 
 			newData, err := json.MarshalIndent(taskList, "", " ")
 
